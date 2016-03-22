3fe720eb36c8a4843273ee0c00772429f216880f
Tanya Pristupova <zyrk@zyrk.net>
2016-03-22 11:08:28 +0100

    Adds sample layout

--- src/WpfOfficeTheme/MainWindow.xaml.cs
+++ src/WpfOfficeTheme/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
 
 namespace WpfOfficeTheme
 {
@@ -7,5 +9,36 @@ namespace WpfOfficeTheme
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<User> Users { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Users = new List<User>
+            {
+                new User {Id = 1, Name = "John", PhoneNumber = 777666880},
+                new User {Id = 2, Name = "Jane", PhoneNumber = 777666881},
+                new User {Id = 3, Name = "Jimmy", PhoneNumber = 777666882},
+                new User {Id = 4, Name = "Mary", PhoneNumber = 777666883},
+                new User {Id = 5, Name = "Simon", PhoneNumber = 777666884},
+                new User {Id = 6, Name = "Ann", PhoneNumber = 777666885},
+                new User {Id = 1, Name = "John", PhoneNumber = 777666880},
+                new User {Id = 2, Name = "Jane", PhoneNumber = 777666881},
+                new User {Id = 3, Name = "Jimmy", PhoneNumber = 777666882},
+                new User {Id = 4, Name = "Mary", PhoneNumber = 777666883},
+                new User {Id = 5, Name = "Simon", PhoneNumber = 777666884},
+                new User {Id = 6, Name = "Ann", PhoneNumber = 777666885}
+            };
+            ListView.ItemsSource = Users;
+            DataGrid.ItemsSource = Users;
+            DataGrid1.ItemsSource = Users;
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
     }
 }
