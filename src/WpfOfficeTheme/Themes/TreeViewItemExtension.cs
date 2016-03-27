05a7d09ac9f5583542a7d888b4e280db42464ca2
Tanya Pristupova <zyrk@zyrk.net>
2016-03-27 12:02:45 +0200

    Adds TreeView style

---
+++ src/WpfOfficeTheme/Themes/TreeViewItemExtension.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfOfficeTheme
+{
+    public static class TreeViewItemExtension
+    {
+        public static int GetDepth(this TreeViewItem item)
+        {
+            var parent = GetParent(item);
+            while ((GetParent(item)) != null)
+            {
+                return GetDepth(parent) + 1;
+            }
+            return 0;
+        }
+
+        public static TreeViewItem GetParent(this TreeViewItem item)
+        {
+            var parent  = VisualTreeHelper.GetParent(item);
+            while (!(parent is TreeViewItem || parent is TreeView))
+            {
+                if (parent == null) return null;
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return parent as TreeViewItem;
+        }
+    }
+}
